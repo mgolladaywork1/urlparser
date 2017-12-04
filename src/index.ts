7a5172cfb0c7e6849da84f2bfc51f5428a6ed6a9
Miu Golladay <mgolladay@snap-software.com>
2017-12-03 21:33:19 -0800

    add parsing to parser

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,7 @@
+import {UrlParser} from './url-parser';
+
 console.log('testing good job');
+const url: string = '/inbox;a=1/33;b=2/(messages/44//help:messages/123)(123:message/4344)?qparam1=2&qparam2=3#main-module';
+console.log(`url is: ${url}`);
+const parser : UrlParser = new UrlParser();
+parser.parser(url);
---
+++ src/url-parser.ts
@@ -0,0 +1,77 @@
+import { DefaultUrlSerializer } from '@angular/router';
+import { UrlTree } from '@angular/router';
+import { UrlSegmentGroup } from '@angular/router';
+import { ParamMap } from '@angular/router/src/shared';
+import { forEach } from '@angular/router/src/utils/collection';
+import { UrlSegment } from '@angular/router/src/url_tree';
+
+export class UrlParser {
+  constructor() { }
+
+  public parser(url: string): void {
+    let serializer: DefaultUrlSerializer = new DefaultUrlSerializer();
+
+    const urlTree: UrlTree = serializer.parse(url);
+    const primaryGroup: UrlSegmentGroup = urlTree.root.children['primary'];
+
+    const groupMap: { [id: string]: UrlSegmentGroup; } = urlTree.root.children;
+    console.log(`urlTree serialized: ${urlTree.toString()}`);
+    console.log(`urlTree serialized: ${(new DefaultUrlSerializer()).serialize(urlTree)}`);
+    const qryParam : ParamMap = urlTree.queryParamMap;
+    console.log(qryParam.keys.length);
+    console.log(` QueryParams: ${JSON.stringify(qryParam)}`);
+    if (qryParam.keys.length > 0 ){
+      qryParam.keys.forEach(qkey => {
+        console.log(` ${qkey} = ${qryParam.getAll(qkey)} `);
+      })
+    }
+    const fragment : string | null = urlTree.fragment;
+    console.log(` url has fragment: ${fragment}`)
+    Object.keys(groupMap).forEach(key => {
+      if (groupMap.hasOwnProperty(key)) {
+        var segmentGroup = groupMap[key];
+        console.log(`${key} has ${segmentGroup.segments.length} segments.`)
+        console.log(`  segments: ${segmentGroup.segments.toString()}`)
+        segmentGroup.segments.forEach(element => {
+          console.log(`    ${element.path}`);
+          console.log(`    ${JSON.stringify(element.parameters)}`);
+          if (element.parameterMap.keys.length > 0) {
+            element.parameterMap.keys.forEach(pkey => {
+              console.log(`      - ${pkey} =  ${element.parameterMap.get(pkey)}`);
+            })
+          }  
+        });
+
+        if (segmentGroup.hasChildren && segmentGroup.numberOfChildren > 0) {
+          console.log(`    ...This segmentGroup has children`);
+          console.log(`       ${segmentGroup.numberOfChildren}`);
+          console.log(`     Need to find primary-->messages/44  //  secondary --> help:messages/123`)
+          let group1Map: { [id: string]: UrlSegmentGroup; } = segmentGroup.children;
+          if (group1Map.hasOwnProperty(key)) {
+            var segment1Group = group1Map[key];
+            console.log(`  ${key} has ${segment1Group.segments.length} segments.`)
+            console.log(`    segments: ${segment1Group.segments.toString()}`)
+            segment1Group.segments.forEach(element => {
+              console.log(`      ${element.path}`);
+              console.log(`      ${JSON.stringify(element.parameters)}`);
+              if (element.parameterMap.keys.length > 0) {
+                element.parameterMap.keys.forEach(pkey => {
+                  console.log(`        - ${pkey} =  ${element.parameterMap.get(pkey)}`);
+                })
+              }  
+            });
+           }
+          }
+      }
+    });
+  }
+
+  private parseGroup(group: { [id: string]: UrlSegmentGroup; } ): void {
+    if (group.children ) {
+      console.log('loop each child');
+      
+    } else {
+      console.log('this is a leaf');
+    }
+  }
+}
